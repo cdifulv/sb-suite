b517d45635137e201f4c2d88a3ddb6851b23a0b7
Craig DiFulvio <42616347+cdifulv@users.noreply.github.com>
2024-07-30 14:42:54 -0400

    init

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,16 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
   plugins: ["prettier-plugin-tailwindcss"],
+  arrowParens: "always",
+  bracketSpacing: true,
+  endOfLine: "auto",
+  printWidth: 80,
+  quoteProps: "as-needed",
+  trailingComma: "none",
+  useTabs: false,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
 };
 
 export default config;
